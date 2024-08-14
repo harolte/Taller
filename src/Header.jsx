a1102607772eaf97e077a6f261c09cb46dcbd721
harolte <harolte@hotmail.com>
2024-08-14 11:24:05 -0500

    agregnado

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,34 @@
-import React from "react"
-export default function Header() {
-    return (
-        <div className="p-5 bg-green-300 text-white -text-3xl font-bold">
-            Micro Frontend2 Header
-        </div>
-    )
-}
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { useNavigate } from "react-router-dom";
+
+const Taller = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    window.location.href = 'https://main.d279we5xa4psmh.amplifyapp.com/'; 
+  };
+
+  return (
+    <div className="text-center">
+      <Header />
+      <img
+        src="https://i.ibb.co/BGs3hHY/metaverso.jpg"
+        className="rounded-full w-32 mb-4 mx-auto"
+        alt="Taller"
+      />
+      <h5 className="text-xl font-medium leading-tight mb-2">Adios Mundo</h5>
+      <p className="text-gray-500">Harolt Kruchinsky</p>
+      <button
+        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={handleGoHome}
+      >
+        Regresar a Home
+      </button>
+      <Footer />
+    </div>
+  );
+};
+
+export default Taller;
